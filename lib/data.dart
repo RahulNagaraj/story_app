df47b39a3739019e40ecf763e3c73706dec7d5ad
Rahul N <rahul.nagarajmv@gmail.com>
2020-04-11 21:59:42 +0530

    display stories

---
+++ lib/data.dart
@@ -0,0 +1,13 @@
+List<String> images = [
+  'assets/images/image_04.jpg',
+  'assets/images/image_03.jpg',
+  'assets/images/image_02.jpg',
+  'assets/images/image_01.png',
+];
+
+List<String> title = [
+  'Hounded Ground',
+  'Fallen in Love',
+  'The Dreaming Moon',
+  'Jack the Persian and the Black Castel',
+];
